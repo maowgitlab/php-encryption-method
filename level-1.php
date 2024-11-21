bec59153eeab988138f1b8fa4ce8403f5495a0b5
maowgit-lab <robianoor2002@gmail.com>
2024-11-21 23:25:27 +0800

    add decryption method

--- level-1.php
+++ level-1.php
@@ -3,3 +3,8 @@
     {
         return openssl_encrypt($plaintext, 'aes-256-cbc', $key, 0, $iv);
     }
+
+    function decrypt($ciphertext, $key, $iv)
+    {
+        return openssl_decrypt($ciphertext, 'aes-256-cbc', $key, 0, $iv);
+    }
